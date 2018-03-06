7b86eccad61197d24777d155f89164c0b1fd78e5
AlejandroVC67 <vcalejandro97@gmail.com>
2018-03-06 16:54:04 -0500

    currently returning navbar structure, UNCOMPLETED

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -18,21 +18,39 @@ export default function NavBar ({content}) {
   console.log(singleLinks) */
 
   return (
-    <nav className="nav-bar">
+    <nav className='nav-bar'>
       <span className='nav-bar__menu'>
         <button className='nav-bar__menu__button'>
-          <div className='nav-bar__menu__button-first'></div>
-          <div className='nav-bar__menu__button-middle'></div>
-          <div className='nav-bar__menu__button-last'></div>          
+          <div className='nav-bar__menu__button-first' />
+          <div className='nav-bar__menu__button-middle' />
+          <div className='nav-bar__menu__button-last' />
         </button>
       </span>
       <div className='nav-bar__image__container'>
         <a className='nav-bar__image-link' href={content.logo.link}>
-          <img className='nav-bar__image' src={content.logo.img} alt='company logo'/>
+          <img className='nav-bar__image' src={content.logo.img} alt='company logo' />
         </a>
       </div>
       <ul className='nav-bar__list'>
-        
+        {
+          content.links.forEach(element => {
+            if (element.hasOwnProperty('links')) {
+              return (
+                <div>
+                  <li className='nav-bar__list-item'>
+                    <a className='nav-bar__list-item-element' href={content.element.href}>{content.element.label}</a>
+                  </li>
+                </div>
+              )
+            } else {
+              return (
+                <div clasName='nav-bar__list-container'>
+                  <li className='nav-bar__list-item'>hola</li>
+                </div>
+              )
+            }
+          })
+        }
       </ul>
     </nav>
   )
